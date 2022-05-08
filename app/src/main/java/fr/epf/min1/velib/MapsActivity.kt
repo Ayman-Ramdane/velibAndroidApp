a8280d3f6380337372b566e920215fb2d92c523d
roxane vellen <roxane.vellen@epfedu.fr>
2022-05-08 17:16:15 +0200

    Regroup icons and icon customisation
    Cluster for less icon when zoom out
    Blue bike for icon

--- app/src/main/java/fr/epf/min1/velib/MapsActivity.kt
+++ app/src/main/java/fr/epf/min1/velib/MapsActivity.kt
@@ -3,14 +3,19 @@ package fr.epf.min1.velib
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.ClusterManager
 import fr.epf.min1.velib.api.LocalisationStation
+import fr.epf.min1.velib.api.Station
 import fr.epf.min1.velib.databinding.ActivityMapsBinding
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
@@ -23,6 +28,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    private lateinit var listStations: List<Station>
+
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.marker)
+        BitmapHelper.vectorToBitmap(this, R.drawable.ic_pedal_bike, color)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -33,6 +45,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        mapFragment.getMapAsync{ googleMap ->
+            addClusteredMarkers(googleMap)
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+                listStations.forEach { bounds.include(LatLng(it.lat, it.lon)) }
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+            }
+        }
+    }
+
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        val clusterManager = ClusterManager<Station>(this, googleMap)
+        clusterManager.renderer =
+            StationRenderer(
+                this,
+                googleMap,
+                clusterManager
+            )
+
+        // Add the places to the ClusterManager.
+        clusterManager.addItems(listStations)
+        clusterManager.cluster()
+
+        // Set ClusterManager as the OnCameraIdleListener so that it
+        // can re-cluster when zooming in and out.
+        googleMap.setOnCameraIdleListener {
+            clusterManager.onCameraIdle()
+        }
     }
 
     /**
@@ -61,13 +101,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val service = retrofit.create(LocalisationStation::class.java)
 
         runBlocking {
-            var listStations = service.getStations().data.stations
-            for (station in listStations){
+            listStations = service.getStations().data.stations
+            //mMap.setMinZoomPreference(12F)
+            /*for (station in listStations){
                 val coordinate = LatLng(station.lat, station.lon)
-                mMap.addMarker(MarkerOptions().position(coordinate).title(station.name))
+                mMap.addMarker(MarkerOptions().position(coordinate).title(station.name).icon(bicycleIcon))
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinate))
                 mMap.setMinZoomPreference(12F)
-            }
+            }*/
         }
 
     }
--- app/src/main/java/fr/epf/min1/velib/api/LocalisationStation.kt
+++ app/src/main/java/fr/epf/min1/velib/api/LocalisationStation.kt
@@ -1,5 +1,7 @@
 package fr.epf.min1.velib.api
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,4 +13,11 @@ interface LocalisationStation {
 
 data class GetStationResult(val data: DataStation)
 data class DataStation(val stations: List<Station>)
-data class Station(val station_id: Double, val name: String, val lat: Double, val lon: Double)
+data class Station(val station_id: Double, val name: String, val lat: Double, val lon: Double) :
+    ClusterItem {
+    override fun getPosition(): LatLng = LatLng(lat, lon)
+
+    override fun getTitle(): String = name
+
+    override fun getSnippet(): String = station_id.toString()
+}
